e798de279edc9f694e0bcd5dfa38c60ef7b14f80
Crevan <a.siverskii@gmail.com>
2023-06-04 18:58:39 +0300

    [add] tests

---
+++ test/com/crevan/task/ProductsImplTest.java
@@ -0,0 +1,103 @@
+package com.crevan.task;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsImplTest {
+
+    private ProductsImpl products;
+
+    @BeforeEach
+    void init(TestInfo info) {
+        products = new ProductsImpl();
+    }
+
+    @ParameterizedTest
+    @MethodSource("getLegalProducts")
+    void addProduct(final Product product) {
+        assertTrue(products.addProduct(product));
+    }
+
+    @Test
+    void addNullProduct() {
+        assertFalse(products.addProduct(null));
+    }
+
+    @Test
+    void deleteProduct() {
+        populateBD();
+        assertTrue(products.deleteProduct(TestData.p1));
+    }
+
+    @Test
+    void deleteNonExistentProduct() {
+        populateBD();
+        assertFalse(products.deleteProduct(TestData.notExist));
+    }
+
+    @Test
+    void deleteNullProduct() {
+        assertFalse(products.deleteProduct(null));
+    }
+
+    @Test
+    void getName() {
+        populateBD();
+        assertEquals("test product 1", products.getName(TestData.ONE));
+    }
+
+    @Test
+    void getNameWhenIdIsNull() {
+        populateBD();
+        assertThrows(IllegalArgumentException.class, () -> products.getName(null));
+    }
+
+    @Test
+    void getNameWhenIdNotExist() {
+        populateBD();
+        assertEquals("", products.getName(TestData.NON_EXISTENT_ID));
+    }
+
+    @Test
+    void findByName() {
+        populateBD();
+        products.addProduct(new Product("test product 1")); // make duplicate
+        List<String> expected = List.of(TestData.ONE, TestData.FOUR);
+        assertEquals(expected, products.findByName(TestData.p1.getName()));
+    }
+
+    @Test
+    void findByNullName() {
+        assertThrows(IllegalArgumentException.class, () -> products.findByName(null));
+    }
+
+    @Test
+    void findByNonExistentName() {
+        populateBD();
+        List<String> expected = Collections.emptyList();
+        assertEquals(expected, products.findByName(TestData.notExist.getName()));
+    }
+
+    private void populateBD() {
+        products.addProduct(TestData.p1);
+        products.addProduct(TestData.p2);
+        products.addProduct(TestData.p3);
+    }
+
+    private static Stream<Product> getLegalProducts() {
+        return Stream.of(
+                TestData.p1,
+                TestData.p2,
+                TestData.p3
+        );
+    }
+}
---
+++ test/com/crevan/task/TestData.java
@@ -0,0 +1,13 @@
+package com.crevan.task;
+
+public class TestData {
+
+    static final Product p1 = new Product("test product 1");
+    static final Product p2 = new Product("test product 2");
+    static final Product p3 = new Product("test product 3");
+    static final Product notExist = new Product("NOT_EXISTED");
+
+    static final String ONE = "1";
+    static final String FOUR = "4";
+    static final String NON_EXISTENT_ID = "0";
+}
